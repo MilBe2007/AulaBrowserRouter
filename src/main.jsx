aab312282e7b61499c2873ee3e8ef5a95f2a8b41
MilBe2007 <mileny.santos@estudante.ifms.edu.br>
2024-08-14 16:06:11 -0400

    return

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/oferta" element={<oferta/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
